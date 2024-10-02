8cc26f95204d668812e8f85a2e9fcd07f8e6e66a
Moin Hasan <111106063+DrMoinHasan@users.noreply.github.com>
2024-10-02 16:41:18 +0530

    Committed

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! This is my Node.js web application.');
+  console.log('Received a request on /');
+  res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`App listening at http://0.0.0.0:${port}`);
 });
